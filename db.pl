3c9d64df253f5d12c7c348df47ffb2e9c914ff78
soh-i <soh.ishiguro3914@gmail.com>
2013-04-17 01:33:50 +0900

    Initial commit

---
+++ db.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use IO::File;
+use Data::Dumper;
+use DBI;
+
+$| = 1;
+
+my $database    = shift or die "SQLite is not found:$!";
+my $data_source = "dbi:SQLite:dbname=$database";
+
+my $dbh = DBI->connect($data_source) or die $DBI::errstr;
+
+# Retrive the SQL table name
+my @names  = $dbh->tables();
+(my $table) = $names[0] =~ m/\.\"(.+)\"/;
+
+# Parsing refFlat file
+my $refFlat = '/home/soh.i/nucRNA/data/Drosophila_melanogaster/UCSC/dm3/Annotation/Genes/refFlat.txt';
+my $data = {};
+my $refflat_io = IO::File->new($refFlat, 'r') or die $!;
+while (my $line = $refflat_io->getline) {
+    my ($gene_id,
+        $tx_id,
+        $chr,
+        $strand,
+        $txStart,
+        $txEnd,
+        $cdsStart,
+        $cdsEnd,
+        $exonCount,
+        $exonStarts,
+        $exonEnds,
+       ) = (split /\t/, $line)[0..10];
+    
+    $data->{source}->{desc}      = "Parsing from refFlat.txt, Archive-2013-03-06-11-04-22 (iGenomes)";
+    $data->{source}->{url}       = 'ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Drosophila_melanogaster/UCSC/dm3/Drosophila_melanogaster_UCSC_dm3.tar.gz';
+    $data->{$tx_id}->{gene_id}   = $gene_id;
+    $data->{$tx_id}->{tx_id}     = $tx_id;
+    $data->{$tx_id}->{chr}       = $chr;
+    $data->{$tx_id}->{txStart}   = $txStart;
+    $data->{$tx_id}->{txEnd}     = $txEnd;
+    $data->{$tx_id}->{exonCount} = $exonCount;
+    #$data->{$tx_id}->{exonStart} = $exonStarts;
+    #$data->{$tx_id}->{exonEnds}  = $exonEnds;
+
+    my @start = split /,/, $exonStarts;
+    my @end   = split /,/, $exonEnds;
+    my $chck = 0;
+    for my $join (0..$exonCount-1) {
+        push @{ $data->{$tx_id}->{exonStart} }, $start[$join];
+        push @{ $data->{$tx_id}->{exonEnd} }, $end[$join];
+        $chck++;
+    }
+    die "[Parsing error]:$!" if $chck ne $exonCount;
+
+}
+$refflat_io->close;
+
+my $gene_coverage = 0;
+my $exon_coverage = 0;
+
+
+for my $key ( keys %{$data} ) {
+    
+    # Fetch as gene (Full length of trancsript)
+    my $sth = $dbh->prepare("select * from $table where chr=\'$data->{$key}->{chr}\' and pos between $data->{$key}->{txStart} and $data->{$key}->{txEnd};");
+    $sth->execute or die $sth->errstr;
+    while (my $gene = $sth->fetchrow_arrayref) {
+        my ($chr, $pos, $depth) = @{ $gene };
+        $gene_coverage += $depth;
+    }
+
+    # Fetch as exon 
+    next if $data->{$key}->{exonCount} < 0;
+    for (my $i = 0; $i<$data->{$key}->{exonCount}; $i++) {
+        $sth = $dbh->prepare("select * from $table where chr=\'$data->{$key}->{chr}\' and pos between $data->{$key}->{exonStart}[$i] and $data->{$key}->{exonEnd}[$i];");
+        $sth->execute or die $sth->errstr;
+        
+        while (my $BED = $sth->fetchrow_arrayref) {
+            my ($chr, $pos, $depth) = @{ $BED };
+            $exon_coverage += $depth;
+        }
+    }
+    
+    print $data->{$key}->{gene_id}, ":", $data->{$key}->{tx_id},"\t";
+    print $gene_coverage, "\t";
+    print $exon_coverage, "\t";
+    print $gene_coverage - $exon_coverage, "\t";
+    print _calculate_SE($gene_coverage, $exon_coverage), "\t";
+    print "\n";
+}
+$dbh->disconnect;
+
+
+sub _calculate_SE {
+    my $gene_cov = shift;
+    my $exon_cov = shift;
+    if ($gene_cov > 0 && $exon_cov > 0) {
+        my $intron_cov = $gene_cov - $exon_cov;
+        return scalar ($intron_cov/$gene_cov);
+    } else { return "None" }
+}
